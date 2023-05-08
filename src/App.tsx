2378ba571bf63600596f426aa495060c69231d44
carolsouza <caah.souza@live.com>
2023-05-07 22:33:56 -0300

    useForms + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
+// import { Button } from './components/Button'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      {/* <Button variant="primary" />
       <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button />
+      <Button variant="danger" /> */}
       <GlobalStyle />
     </ThemeProvider>
   )
